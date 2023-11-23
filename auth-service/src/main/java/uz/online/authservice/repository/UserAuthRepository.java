4eea31558364700ed66933443b44e190696442d4
jabborovjaloliddin9 <jabborovjalol9@gmail.com>
2023-11-24 00:46:01 +0500

    Initial commit

--- auth-service/src/main/java/uz/online/authservice/repository/UserAuthRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.online.authservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.online.authservice.entity.UserAuth;
-
-public interface UserAuthRepository extends JpaRepository<UserAuth, Integer> {
-}
